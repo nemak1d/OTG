3f5212f7a22cc6f7e587db266802ddcbd93f5380
nemak1d <nmk1dbaddf00d@gmail.com>
2020-01-25 03:32:10 +0900

    編集不可アトリビュート追加

---
+++ Assets/Scripts/Framework/EditorExt/UneditableAttribute.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace N1D
+{
+	public sealed class UneditableAttribute : PropertyAttribute { }
+#if UNITY_EDITOR
+	[CustomPropertyDrawer(typeof(UneditableAttribute))]
+	public sealed class UneditableAttributeDrawer : PropertyDrawer
+	{
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			return EditorGUI.GetPropertyHeight(property, label, true);
+		}
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			GUI.enabled = false;
+			EditorGUI.PropertyField(position, property, label, true);
+			GUI.enabled = true;
+		}
+	}
+#endif
+} // N1D
