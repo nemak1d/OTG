c47bc3ce11e41e265ce41cd37f851f34834e0030
nemak1d <nmk1dbaddf00d@gmail.com>
2020-01-25 03:32:55 +0900

    ベースライン作成中

--- Assets/Scripts/App/Mock/TimingLine.cs
+++ Assets/Scripts/App/Mock/TimingLine.cs
@@ -1,49 +1,166 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using N1D.Dbg;
+using N1D.Framework.Core;
+using N1D.Framework.Dbg;
 
-public class TimingLine : MonoBehaviour
+namespace N1D.App
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-		m_StartTime = Time.realtimeSinceStartup;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-		DrawLine(m_TimingPoint + (m_ToFromDirection * m_Length), Vector3.right, m_LineWidth, Color.yellow);
-		DrawLine(m_TimingPoint, Vector3.right, m_LineWidth, Color.red);
-
-		UpdateLine();
-	}
-
-	private void UpdateLine()
+	public class TimingLine : MonoBehaviour
 	{
-		var delta = Time.realtimeSinceStartup - m_StartTime;
-		var t = delta % m_LengthMoveTime;
-		var point = Vector3.Lerp(m_TimingPoint + (m_ToFromDirection * m_Length), m_TimingPoint, t);
-		DrawLine(point, Vector3.right, m_LineWidth, Color.green);
+		// Start is called before the first frame update
+		void Start()
+		{
+			m_StartTime = Time.realtimeSinceStartup;
+
+			for (var i = 0; i < 100; ++i)
+			{
+				var guide = new Guide();
+				guide.id = i;
+				m_Guides.Add(guide);
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			m_Interval = CalculateInterval(m_Bpm);
+			m_DestinationTime = CalculateDestinationTime(m_Speed, m_Length);
+
+			DrawLine(m_TimingPoint + (m_ToFromDirection * m_Length), Vector3.right, m_LineWidth, Color.yellow);
+			DrawLine(m_TimingPoint, Vector3.right, m_LineWidth, Color.red);
+
+			UpdateLine();
+		}
+
+		private void UpdateLine()
+		{
+#if false
+			var delta = Time.realtimeSinceStartup - m_StartTime;
+			m_Delta = delta;
+			var t = delta % m_DestinationTime / m_DestinationTime;
+			m_Rate = t;
+			var point = Vector3.Lerp(m_TimingPoint + (m_ToFromDirection * m_Length), m_TimingPoint, t);
+			DrawLine(point, Vector3.right, m_LineWidth, Color.green);
+#else
+			var delta = (Time.realtimeSinceStartup - m_StartTime);
+			var count = (int)(delta / m_Interval);
+			if (count > m_ProceededCount)
+			{
+				foreach (var guide in m_Guides)
+				{
+					if (guide.isActive)
+					{
+						continue;
+					}
+					Debug.Log("start->" + delta);
+					guide.Start(m_Interval * ++m_ProceededCount + m_StartTime);
+					if (count <= ++m_ProceededCount)
+					{
+						break;
+					}
+				}
+			}
+
+			var i = 0;
+			foreach (var guide in m_Guides)
+			{
+				++i;
+				if (!guide.isActive)
+				{
+					continue;
+				}
+				guide.Update(m_DestinationTime);
+				var point = Vector3.Lerp(m_TimingPoint + (m_ToFromDirection * m_Length), m_TimingPoint, guide.t);
+				DrawLine(point, Vector3.right, m_LineWidth, new Color(0.0f, 0.1f * guide.id % 1.0f, 0.0f));
+				Debug.Log(i + "->" + guide.t);
+			}
+
+#endif
+		}
+
+		private void DrawLine(Vector3 point, Vector3 direction, float length, Color color)
+		{
+			var half = direction * length;
+			var from = point - half;
+			var to = point + half;
+
+			Draw.instance.Line(from, to, color);
+		}
+
+		private float CalculateInterval(float bpm)
+		{
+			Debug.Assert(!bpm.IsZero());
+
+			return	60.0f / bpm;
+		}
+
+		private float CalculateDestinationTime(float speed, float length)
+		{
+			Debug.Assert(!speed.IsZero());
+
+			return length / speed;
+		}
+
+		// positions
+		public Vector3 m_TimingPoint = Vector3.zero;
+		public Vector3 m_ToFromDirection = Vector3.up;
+		public float m_Length = 5.0f;
+
+		// parameter
+		public float m_Speed = 1.0f;
+
+		// visual
+		public float m_LengthMoveTime = 1.0f;
+		public float m_LineWidth = 3.0f;
+
+		public float m_StartTime = 0.0f;
+
+		[SerializeField, Range(0.001f, 9999.0f)]
+		private float m_Bpm = 120.0f;
+
+		[SerializeField, Uneditable]
+		private float m_Interval = 0.0f;
+		[SerializeField, Uneditable]
+		private float m_DestinationTime = 0.0f;
+		[SerializeField, Uneditable]
+		private float m_Delta = 0.0f;
+		[SerializeField, Uneditable]
+		private float m_Rate = 0.0f;
+		[SerializeField, Uneditable]
+		private int m_Count = 0;
+		private int m_ProceededCount = 0;
+
+		// スポーン間隔は決まってるので、後は何秒でタイミングになるか…
+		List<Guide> m_Guides = new List<Guide>();
+
+		private class Guide
+		{
+			public void Start(float startTime)
+			{
+				m_StartTime = startTime;
+				isActive = true;
+			}
+
+			public void Update(float destinationTime)
+			{
+				if (!isActive)
+				{
+					return;
+				}
+
+				var delta = Time.realtimeSinceStartup - m_StartTime;
+				t = delta % destinationTime / destinationTime;
+
+				if (delta >= destinationTime)
+				{
+					isActive = false;
+				}
+			}
+			public int id { set; get; }
+			public float t { private set; get; } = 0.0f;
+			public bool isActive { private set; get; } = false;
+
+			private float m_StartTime = 0.0f;
+		}
 	}
-
-	private void DrawLine(Vector3 point, Vector3 direction, float length, Color color)
-	{
-		var half = direction * length;
-		var from = point - half;
-		var to = point + half;
-
-		Draw.instance.Line(from, to, color);
-	}
-
-	public Vector3 m_TimingPoint = Vector3.zero;
-	public Vector3 m_ToFromDirection = Vector3.up;
-	public float m_Length = 5.0f;
-
-	public float m_LengthMoveTime = 1.0f;
-	public float m_LineWidth = 3.0f;
-
-	public float m_StartTime = 0.0f;
 }
