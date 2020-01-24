1feb8657289065086383889a85288244e80176d6
nemak1d <nmk1dbaddf00d@gmail.com>
2020-01-25 03:32:40 +0900

    float.IsZero追加

---
+++ Assets/Scripts/Framework/Core/MathExtension.cs
@@ -0,0 +1,16 @@
+﻿
+namespace N1D.Framework.Core
+{
+	public static class MathExtension
+	{
+		public const float LocalEpsilon = 0.0001f;
+
+		public static bool IsZero(this float val)
+		{
+			return -LocalEpsilon < val && val < LocalEpsilon;
+		}
+
+
+		
+	}
+} // N1D.Framework.Core
