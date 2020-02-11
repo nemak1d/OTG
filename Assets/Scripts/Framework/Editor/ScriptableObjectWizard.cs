8231d412e02a81a906b3bf33a9021cf18b48538e
nemak1d <nmk1dbaddf00d@gmail.com>
2020-02-11 12:40:56 +0900

    ScriptableObject生成機能追加

---
+++ Assets/Scripts/Framework/Editor/ScriptableObjectWizard.cs
@@ -0,0 +1,85 @@
+﻿//-------------------------------------------
+//
+//  ScriptableObjectWizard
+//
+//-------------------------------------------
+using UnityEngine;
+using UnityEditor;
+
+namespace N1D.Framework.Editor
+{
+    public class ScriptableObjectWizard : ScriptableWizard
+    {
+        //-----------------------------------
+        // MonoBehaviour
+        //-----------------------------------
+		private void OnWizardCreate()
+		{
+			if (m_CreateType == ScriptableObjectType.None)
+			{
+				Debug.LogWarning("生成対象のScriptableObjectTypeを指定してください.");
+				return;
+			}
+
+			var path = EditorUtility.SaveFilePanelInProject("Save", m_CreateType.ToString(), "asset", "ファイル名を入力して保存先を選択してください");
+			if (string.IsNullOrEmpty(path))
+			{
+				return;
+			}
+
+			switch (m_CreateType)
+			{
+				case ScriptableObjectType.TrackSheet:
+				{
+					CreateAsset<App.TrackSheet>(path);
+					break;
+				}
+
+				default:
+				{
+					break;
+				}
+			}
+		}
+		//-----------------------------------
+		// Method (private)
+		//-----------------------------------
+		[MenuItem("Tools/Create Scriptable Object")]
+		private static void Open()
+		{
+			GetWindow<ScriptableObjectWizard>("Create Scriptable Object Wizard");
+		}
+
+		private static void CreateAsset<T>(string path) where T : ScriptableObject
+		{
+			Debug.Assert(!string.IsNullOrEmpty(path));
+
+			T instance = CreateInstance<T>();
+
+			AssetDatabase.CreateAsset(instance, path);
+			AssetDatabase.SaveAssets();
+		}
+		//-----------------------------------
+		// Property
+		//-----------------------------------
+
+		//-----------------------------------
+		// Define
+		//-----------------------------------
+		public enum ScriptableObjectType : int
+		{
+			None = 0,
+			TrackSheet,
+		}
+
+		//-----------------------------------
+		// Field
+		//-----------------------------------
+		[SerializeField]
+		private ScriptableObjectType m_CreateType = ScriptableObjectType.None;
+
+        //-----------------------------------
+        // Internal Class / Struct
+        //-----------------------------------
+    }
+} // N1D.Framework.Editor
